e5df174fc54323bca9074321c4c7719b2954d309
Joseph Frazier <1212jtraceur@gmail.com>
2017-10-07 08:34:25 -0400

    Lift addresses state up to top

--- src/App.js
+++ src/App.js
@@ -7,17 +7,41 @@ class App extends Component {
   rows = ['A', 'B', 'C', 'D', 'E']
   cols = [1, 2, 3, 4, 5]
 
+  constructor (props) {
+    super(props)
+
+    this.state = this.addGrid('', {
+      origin: '',
+      destination: '',
+      babyFood1: '',
+      babyFood2: '',
+    })
+  }
+
+  addGrid (value, result) {
+    return this.rows.reduce((result, row) => {
+      return this.cols.reduce((result, col) => {
+        result[row + col] = ''
+        return result
+      }, result)
+    }, result)
+  }
+
   render() {
     return (
       <div className="App">
         <SimpleForm
           inputProps={{
+            value: this.state.origin,
+            onChange: address => this.setState({ origin: address }),
             placeholder: 'Origin'
           }}
         />
 
         <SimpleForm
           inputProps={{
+            value: this.state.destination,
+            onChange: address => this.setState({ destination: address }),
             placeholder: 'Destination'
           }}
         />
@@ -47,6 +71,8 @@ class App extends Component {
                       <td key={col}>
                         <SimpleForm
                           inputProps={{
+                            value: this.state[row + col],
+                            onChange: address => this.setState({ [row + col]: address }),
                             placeholder: row + col
                           }}
                         />
@@ -64,6 +90,8 @@ class App extends Component {
             <SimpleForm
               key={n}
               inputProps={{
+                value: this.state['babyFood' + n],
+                onChange: address => this.setState({ ['babyFood' + n]: address }),
                 placeholder: 'Baby Food Stop ' + n
               }}
             />
