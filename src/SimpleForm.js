08952eb0a41d233f7f82d9479f451efd6e299802
Joseph Frazier <1212jtraceur@gmail.com>
2017-10-07 07:16:57 -0400

    move focused input to top, highlight and full-width

--- src/SimpleForm.js
+++ src/SimpleForm.js
@@ -6,21 +6,33 @@ class SimpleForm extends React.Component {
     super(props)
     this.state = {}
     this.onChange = (address) => this.setState({ address })
+    this.onFocus = () => this.setState({ focused: true })
+    this.onBlur = () => this.setState({ focused: false })
   }
 
   render() {
     const inputProps = Object.assign({}, this.props.inputProps, {
       value: this.state.address,
       onChange: this.onChange,
+      onFocus: this.onFocus,
+      onBlur: this.onBlur,
     })
 
     return (
       <PlacesAutocomplete
         inputProps={inputProps}
         styles={{
-          autocompleteContainer: {
-            zIndex: 1
-          }
+          root: this.state.focused ? {
+            position: 'fixed',
+            top: '1rem',
+            left: 0,
+            width: '100%',
+            zIndex: 1,
+          } : {},
+
+          input: this.state.focused ? {
+            background: 'lightyellow',
+          } : {},
         }}
       />
     )
