f302e34330b04a2a4c4da1aacac97cc3022b32e2
Joseph Frazier <1212jtraceur@gmail.com>
2017-10-07 12:36:34 -0400

    Default to addresses in Cranksgiving 11 manifest
    https://farm3.static.flickr.com/2778/4134507221_d0c9ec1b7c_o.jpg

--- src/App.js
+++ src/App.js
@@ -10,12 +10,51 @@ class App extends Component {
   constructor (props) {
     super(props)
 
-    this.state = this.addGrid('', {
-      origin: '',
-      destination: '',
-      babyFood1: '',
-      babyFood2: '',
-    })
+    // this.state = this.addGrid('', {
+    //   origin: '',
+    //   destination: '',
+    //   babyFood1: '',
+    //   babyFood2: '',
+    // })
+
+    // https://farm3.static.flickr.com/2778/4134507221_d0c9ec1b7c_o.jpg
+    this.state = {
+      'origin': 'Hudson Yards Park',
+      'destination': '440 Grand St',
+
+      'A1': '221-225 8th Ave, 10011',
+      'A2': '907 8th Ave, NYC',
+      'A3': '289 Columbus Ave, NYC',
+      'A4': '25 University Pl, NYC',
+      'A5': '',
+
+      'B1': '512 2nd Ave, NYC',
+      'B2': '452 W 43rd St., NYC',
+      'B3': '1407 Lexington Ave, NYC',
+      'B4': '316 Greenwich St, NYC',
+      'B5': '',
+
+      'C1': '311 E 23rd St, NYC',
+      'C2': '580 9th Ave, NYC',
+      'C3': '2704 Broadway, NYC',
+      'C4': '5 St. James Pl, NYC',
+      'C5': '',
+
+      'D1': '10 Union Sq. East, NYC',
+      'D2': '225 W. 57th St, NYC',
+      'D3': '609 Columbus Ave, NYC',
+      'D4': '2217 7th Ave, NYC',
+      'D5': '',
+
+      'E1': '',
+      'E2': '',
+      'E3': '',
+      'E4': '',
+      'E5': '',
+
+      'babyFood1': '441 West 26th St, NYC',
+      'babyFood2': '137 East 2nd St, NYC'
+    }
   }
 
   addGrid (value, result) {
